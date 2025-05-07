a7fdd4d2a4b2386e86ce23137a37a79ad90288f4
sodiqscript111 <simidusodiqt@gmail.com>
2025-05-07 21:57:51 +0100

    fixing bugs

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,9 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_NODE_ENV === 'production'
-      ? 'https://urbannest-backend.onrender.com'
-      : '/api',
+  baseURL: 'https://urbannest-backend.onrender.com',
 });
 
 export default api;
