42a33d9a4e4763e94b9f0d687f86616f4f3dbc01
sodiqscript111 <simidusodiqt@gmail.com>
2025-05-07 18:20:37 +0100

    Add PropertyCard componen

--- client/src/component/Guestfavorite.jsx
+++ client/src/component/Guestfavorite.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import PropertyCard from './PropertyCard';
+import PropertyCard from './Propertycard.jsx';
 
 const GuestFavorites = () => {
     const [listings, setListings] = useState([]);
