61fd30a9651af4dd2f3b4759f1e5d1f062fb2f77
sodiqscript111 <simidusodiqt@gmail.com>
2025-11-13 17:47:00 +0100

    Refactoring

--- internal/interfaces/store.go
+++ internal/interfaces/store.go
@@ -6,11 +6,22 @@ import (
 )
 
 type Database interface {
+	//Users operations
 	CreateUser(ctx context.Context, user *entities.User) error
 	GetUser(ctx context.Context, id uint) (*entities.User, error)
+
+	//Listings operations
 	CreateListing(ctx context.Context, listing *entities.Listing) error
 	GetListing(ctx context.Context, id uint) (*entities.Listing, error)
 	UpdateListing(ctx context.Context, listing *entities.Listing) error
 	DeleteListing(ctx context.Context, id uint) error
 	CheckAvailability(ctx context.Context, listingID uint, startDate, endDate string) (bool, error)
+
+	//Bookings operations
+	CreateBooking(ctx context.Context, booking *entities.Booking) error
+	GetBooking(ctx context.Context, id uint) (*entities.Booking, error)
+	GetBookingsByUser(ctx context.Context, userID uint) ([]entities.Booking, error)
+	GetBookingsByHost(ctx context.Context, hostID uint) ([]entities.Booking, error)
+	UpdateBooking(ctx context.Context, booking *entities.Booking) error
+	DeleteBooking(ctx context.Context, bookingID uint) error
 }
